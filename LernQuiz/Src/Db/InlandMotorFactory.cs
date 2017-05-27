ccf81825ec804ee6272cf0a510743eb3c37c3444
Florian Michel <michel-florian@gmx.de>
2017-05-27 19:56:51 +0200

    edit db

--- LernQuiz/Src/Db/InlandMotorFactory.cs
+++ LernQuiz/Src/Db/InlandMotorFactory.cs
@@ -11,8 +11,8 @@ namespace LernQuiz.Db
 {
 	public class InlandMotorFactory : IEstablishQuestionnaires
 	{
-        //private String ConnectionString =   "Server =local;" + "Database=test;"+"Integrated Security = sspi";
-        private String  ConnectionString = "Server=carlaischmann.de;" + "Database=Binnenschifffahrt;" + "User ID=smoky;" + "Password=fghk32937fdg!2;" + "Pooling=false";
+        string connectionString = "server=localhost;database=Binnenschifffahrt;Trusted_Connection=True;";       
+        //private String  ConnectionString = "Server=carlaischmann.de;" + "Database=Binnenschifffahrt;" + "User ID=smoky;" + "Password=fghk32937fdg!2;" + "Pooling=false";
 
         public InlandMotorFactory ()
 		{
@@ -25,13 +25,24 @@ namespace LernQuiz.Db
 				"SELECT DISTINCT FragebogenNr " +
 				"FROM T_Fragebogen_unter_Maschine";
 
-			IDataReader reader = this.query (sqlCommand).ExecuteReader();
-			while (reader.Read ()) {
-				string frageBogenId = ((int)reader ["FragebogenNr"]).ToString();
-				fragebogenIds.Add (frageBogenId);
-			}
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sqlCommand, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string frageBogenId = ((int)reader["FragebogenNr"]).ToString();
+                    fragebogenIds.Add(frageBogenId);
+                }
+                reader.Close();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-			return fragebogenIds;
+            return fragebogenIds;
 		}
 			
 		public Questionnaire GetFragebogen (String fragebogenID)
@@ -46,50 +57,66 @@ namespace LernQuiz.Db
 				"ON T_SBF_Binnen.P_Id = T_Fragebogen_unter_Maschine.F_Id_SBF_Binnen  " +
 				"AND T_Fragebogen_unter_Maschine.FragebogenNr = " + fragebogenID + ";";
 
-			IDataReader reader = this.query (sqlCommand).ExecuteReader();
-			while (reader.Read ()) {
-				int questionNumber 	= (int)reader ["P_Id"];
-				string question 	= (string)reader ["Frage"];
-				string answer1 		= (string)reader ["Antwort1"];
-				string answer2 		= (string)reader ["Antwort2"];
-				string answer3 		= (string)reader ["Antwort3"];
-				string answer4 		= (string)reader ["Antwort4"];
-				byte correctAnswer 	= (byte)reader ["RichtigeAntwort"];
 
-				questions.Add (
-					new Question(
-						questionNumber,
-						question,
-						new List<Answer> {
-							new Answer(answer1, correctAnswer == 1),
-							new Answer(answer2, correctAnswer == 2),
-							new Answer(answer3, correctAnswer == 3),
-							new Answer(answer4, correctAnswer == 4)
-						}
-					)
-				);
-			}
+
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sqlCommand, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int questionNumber = (int)reader["P_Id"];
+                    string question = (string)reader["Frage"];
+                    string answer1 = (string)reader["Antwort1"];
+                    string answer2 = (string)reader["Antwort2"];
+                    string answer3 = (string)reader["Antwort3"];
+                    string answer4 = (string)reader["Antwort4"];
+                    byte correctAnswer = (byte)reader["RichtigeAntwort"];
+
+                    questions.Add(
+                        new Question(
+                            questionNumber,
+                            question,
+                            new List<Answer> {
+                            new Answer(answer1, correctAnswer == 1),
+                            new Answer(answer2, correctAnswer == 2),
+                            new Answer(answer3, correctAnswer == 3),
+                            new Answer(answer4, correctAnswer == 4)
+                            }
+                        )
+                    );
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
 			questionnaire = new Questionnaire (questions);
 			return questionnaire;
 		}
 
-		private IDbCommand query(String sqlCommand) {
-            IDbConnection dbcon;
-            dbcon = new MySqlConnection(ConnectionString);
-            dbcon.Open();
+		private SqlDataReader query(String sqlCommand) {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sqlCommand, connection);
+            SqlDataReader reader = null;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-            IDbCommand dbcmd = dbcon.CreateCommand();
-
-           // SqlConnection con;
-            //con = new SqlConnection(ConnectionString);
-            //con.Open();
-
-            //IDbCommand dbcmd = con.CreateCommand();
-
-            dbcmd.CommandText = sqlCommand;
-
-			return dbcmd;
+            return reader;
 		}
 	}
 }
